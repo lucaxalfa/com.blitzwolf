1fabaa551b46d32cafe320d60d2314ff11d74c9e
Marco Gosselink <marco@famgosselink.nl>
2021-03-09 20:42:23 +0100

    Added capabilities and images

--- drivers/bw-shp13/device.js
+++ drivers/bw-shp13/device.js
@@ -3,16 +3,73 @@
 const Homey = require('homey');
 const { ZigBeeDevice } = require('homey-zigbeedriver');
 const { CLUSTER } = require('zigbee-clusters');
-// const { debug } = require('zigbee-clusters');
+const { debug } = require('zigbee-clusters');
 
-// debug(true);
+debug(false);
 
 class bw_shp13 extends ZigBeeDevice {
     async onNodeInit({ zclNode }) {
 
         if (this.hasCapability('onoff')) {
-            this.registerCapability('onoff', CLUSTER.ON_OFF);
-          }
+          this.registerCapability('onoff', CLUSTER.ON_OFF, {
+            getOpts: {
+              getOnStart: true,
+              pollInterval: 60000
+            }
+          });
+        }
+
+        if (this.hasCapability('measure_voltage')) {
+          this.registerCapability('measure_voltage', CLUSTER.ELECTRICAL_MEASUREMENT, {
+              getOpts: {
+                getOnStart: true,
+                pollInterval: 10000
+              },
+              reportParser(value) {
+                return value;
+              },
+              endpoint: 1
+            });
+        }
+
+        if (this.hasCapability('measure_current')) {
+          this.registerCapability('measure_current', CLUSTER.ELECTRICAL_MEASUREMENT, {
+              getOpts: {
+                getOnStart: true,
+                pollInterval: 10000
+              },
+              reportParser(value) {
+                return value / 1000;
+              },
+              endpoint: 1
+            });
+        }
+
+        if (this.hasCapability('measure_power')) {
+          this.registerCapability('measure_power', CLUSTER.ELECTRICAL_MEASUREMENT, {
+              getOpts: {
+                getOnStart: true,
+                pollInterval: 60000
+              },
+              reportParser(value) {
+                return value;
+              },
+              endpoint: 1
+            });
+        }
+
+        if (this.hasCapability('meter_power')) {
+          this.registerCapability('meter_power', CLUSTER.METERING, {
+              getOpts: {
+                getOnStart: true,
+                pollInterval: 300000
+              },
+              reportParser(value) {
+                return value / 1000;
+              },
+              endpoint: 1
+            });
+        }
     }
 }
 
